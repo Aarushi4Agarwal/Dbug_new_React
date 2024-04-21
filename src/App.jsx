f6ffa9e1104cf75a29980774c8efdd1fd644021d
Aarushi Agarwal <132547930+Aarushi4Agarwal@users.noreply.github.com>
2024-04-21 17:53:15 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import Hero from './components/Hero';
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar.jsx";
+import Destinations from "./components/Destinations.jsx";
+import Search from "./components/Search.jsx";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Destinations />
+      <Search />
     </div>
   );
 }
